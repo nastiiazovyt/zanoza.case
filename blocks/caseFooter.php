1682407fd73a790d3e54b63d9d3b09c7b9479b32
Anastasiya Okisheva <nastiiazovyt@gmail.com>
2022-06-23 10:40:49 +0700

    - отверстала футер кейса (без анимации)

--- blocks/caseFooter.php
+++ blocks/caseFooter.php
@@ -1,8 +1,20 @@
 <div class="case-footer">
     <div class="case-footer-container">
+        <div class="case-footer-left-side">
+            <svg class="case-footer-nav" width="70" height="8" viewBox="0 0 70 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 1L1 4L5 7V4V1Z" fill="white"/>
+                <path d="M5 4V7L1 4L5 1V4ZM5 4H69.5" stroke="white"/>
+            </svg>
+        </div>
         <div class="case-footer-title">
-            <span>(Доменное имя сайта клиента)</span>
+            <span>oliviera.org</span>
             <span>Перейти на сайт</span>
+        </div>
+        <div class="case-footer-right-side">
+            <svg class="case-footer-nav" width="70" height="8" viewBox="0 0 70 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 1L1 4L5 7V4V1Z" fill="white"/>
+                <path d="M5 4V7L1 4L5 1V4ZM5 4H69.5" stroke="white"/>
+            </svg>
         </div>
     </div>
 </div>
